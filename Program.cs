b68621568bc49d70815c7d7eed2d8eca586422ba
codedanilo <143664389+codedanilo@users.noreply.github.com>
2023-11-13 15:01:26 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿#region TiposDados
+        int tipoInteiro;
+        double tipoDouble;
+        decimal tipDecimal;
+        long tipoLong;
+
+        tipoInteiro = int.MaxValue;
+
+
+        Console.WriteLine($"Este valor {tipoInteiro} eh o maior inteiro possivel");
+#endregion  
