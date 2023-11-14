7c829181bdcf7976cff249cccfb433f1304335f4
Danilo Silveira <dansilver32@gmail.com>
2023-11-14 10:15:36 -0300

    Subindo exercícios

---
+++ Semana02/divisiveis.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Números divisíveis por 3 ou por 4 entre 0 e 30:");
+
+        for (int i = 0; i <= 30; i++)
+        {
+            if (i % 3 == 0 || i % 4 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        Console.ReadLine(); // Aguarda a entrada do usuário antes de fechar a janela
+    }
+}
+
