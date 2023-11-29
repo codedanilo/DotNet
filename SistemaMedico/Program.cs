5643f7cc375381fa08e9a64ccf905a4b7367ed0c
codedanilo <143664389+codedanilo@users.noreply.github.com>
2023-11-29 12:12:31 -0300

    Finalizado

--- SistemaMedico/Program.cs
+++ SistemaMedico/Program.cs
@@ -1,140 +1,238 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace SistemaMedico;
+public enum Sexo
+{
+    Masculino,
+    Feminino
+}
+
 public class Pessoa
 {
-    public string? Nome { get; set; }
+    public string Nome { get; set; }
     public DateTime DataNascimento { get; set; }
-    public string? CPF { get; set; }
+    public string CPF { get; set; }
+
+    public int Idade
+    {
+        get
+        {
+            int idade = DateTime.Now.Year - DataNascimento.Year;
+            if (DateTime.Now.Month < DataNascimento.Month || (DateTime.Now.Month == DataNascimento.Month && DateTime.Now.Day < DataNascimento.Day))
+            {
+                idade--;
+            }
+            return idade;
+        }
+    }
 }
 
 public class Medico : Pessoa
 {
-    public string? CRM { get; set; }
+    public string CRM { get; set; }
+
+    public Medico(string nome, DateTime dataNascimento, string cpf, string crm)
+    {
+        Nome = nome;
+        DataNascimento = dataNascimento;
+        CPF = cpf;
+        CRM = crm;
+    }
 }
 
 public class Paciente : Pessoa
 {
-    public string? Sexo { get; set; }
-    public string? Sintomas { get; set; }
+    public Sexo Sexo { get; set; }
+    public string Sintomas { get; set; }
+
+    public Paciente(string nome, DateTime dataNascimento, string cpf, Sexo sexo, string sintomas)
+    {
+        Nome = nome;
+        DataNascimento = dataNascimento;
+        CPF = cpf;
+        Sexo = sexo;
+        Sintomas = sintomas;
+    }
 }
 
-public static bool ValidarCPF(string cpf)
+public class ClinicaMedica
 {
-    if (cpf.Length != 11 || !cpf.All(char.IsDigit))
+    private List<Medico> medicos = new List<Medico>();
+    private List<Paciente> pacientes = new List<Paciente>();
+
+    public void AdicionarMedico(Medico medico)
     {
-        return false;
+        
+        if (!medicos.Any(m => m.CPF == medico.CPF) && !medicos.Any(m => m.CRM == medico.CRM))
+        {
+           
+            if (ValidarCPF(medico.CPF))
+            {
+                medicos.Add(medico);
+            }
+            else
+            {
+                throw new ArgumentException("O CPF deve ter exatamente 11 dígitos numéricos.");
+            }
+        }
+        else
+        {
+            throw new ArgumentException("CPF ou CRM já cadastrado para outro médico.");
+        }
     }
 
-    int[] multiplicadoresPrimeiroDigito = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-    int[] multiplicadoresSegundoDigito = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-
-    string cpfSemDigitos = cpf.Substring(0, 9);
-
-    int CalculaDigito(string cpfParcial, int[] multiplicadores)
+    public void AdicionarPaciente(Paciente paciente)
     {
-        int soma = 0;
-        for (int i = 0; i < cpfParcial.Length; i++)
+        
+        if (!pacientes.Any(p => p.CPF == paciente.CPF))
         {
-            soma += int.Parse(cpfParcial[i].ToString()) * multiplicadores[i];
+           
+            if (ValidarCPF(paciente.CPF))
+            {
+                pacientes.Add(paciente);
+            }
+            else
+            {
+                throw new ArgumentException("O CPF deve ter exatamente 11 dígitos numéricos.");
+            }
+        }
+        else
+        {
+            throw new ArgumentException("CPF já cadastrado para outro paciente.");
+        }
+    }
+
+    public IEnumerable<Medico> ObterMedicosPorIdade(int idadeMinima, int idadeMaxima)
+    {
+        return medicos.Where(m => m.Idade >= idadeMinima && m.Idade <= idadeMaxima);
+    }
+
+    public IEnumerable<Paciente> ObterPacientesPorIdade(int idadeMinima, int idadeMaxima)
+    {
+        return pacientes.Where(p => p.Idade >= idadeMinima && p.Idade <= idadeMaxima);
+    }
+
+    public IEnumerable<Paciente> ObterPacientesPorSexo(Sexo sexo)
+    {
+        return pacientes.Where(p => p.Sexo == sexo);
+    }
+
+    public IEnumerable<Paciente> ObterPacientesOrdemAlfabetica()
+    {
+        return pacientes.OrderBy(p => p.Nome);
+    }
+
+    public IEnumerable<Paciente> ObterPacientesPorSintomas(string textoSintomas)
+    {
+        return pacientes.Where(p => p.Sintomas.Contains(textoSintomas, StringComparison.OrdinalIgnoreCase));
+    }
+
+    public IEnumerable<Pessoa> ObterAniversariantesDoMes(int mes)
+    {
+        return medicos.Concat<Pessoa>(pacientes).Where(p => p.DataNascimento.Month == mes);
+    }
+
+    private static bool ValidarCPF(string cpf)
+    {
+        
+        if (cpf.Length != 11 || !cpf.All(char.IsDigit))
+        {
+            return false;
         }
 
-        int resto = soma % 11;
-        return resto < 2 ? 0 : 11 - resto;
+        return true;
     }
-
-    int primeiroDigito = CalculaDigito(cpfSemDigitos, multiplicadoresPrimeiroDigito);
-    cpfSemDigitos += primeiroDigito;
-    int segundoDigito = CalculaDigito(cpfSemDigitos, multiplicadoresSegundoDigito);
-
-    return cpf.EndsWith($"{primeiroDigito}{segundoDigito}");
 }
-
 
 class Program
 {
-    static void Main(string[] args)
+    static void ExibirMedicos(IEnumerable<Medico> medicos)
     {
-        List<Medico> medicos = new List<Medico>();
-        List<Paciente> pacientes = new List<Paciente>();
-
-        try
+        foreach (var medico in medicos)
         {
-            medicos.Add(new Medico { Nome = "Dr. João", DataNascimento = new DateTime(1980, 5, 15), CPF = "12345678900", CRM = "ABC123" });
-            medicos.Add(new Medico { Nome = "Dra. Maria", DataNascimento = new DateTime(1975, 9, 25), CPF = "98765432100", CRM = "XYZ789" });
-
-            pacientes.Add(new Paciente { Nome = "Carlos", DataNascimento = new DateTime(1995, 3, 10), CPF = "11122233344", Sexo = "Masculino", Sintomas = "Dor de cabeça" });
-            pacientes.Add(new Paciente { Nome = "Ana", DataNascimento = new DateTime(1988, 7, 5), CPF = "55566677788", Sexo = "Feminino", Sintomas = "Febre" });
-            pacientes.Add(new Paciente { Nome = "Mariana", DataNascimento = new DateTime(2000, 12, 20), CPF = "99988877766", Sexo = "Feminino", Sintomas = "Tosse" });
-
-            var medicosPorIdade = medicos.Where(m => (DateTime.Now.Year - m.DataNascimento.Year) >= 35 && (DateTime.Now.Year - m.DataNascimento.Year) <= 50);
-            Console.WriteLine("Médicos com idade entre 35 e 50 anos:");
-            foreach (var medico in medicosPorIdade)
-            {
-                Console.WriteLine($"Nome: {medico.Nome}, Idade: {DateTime.Now.Year - medico.DataNascimento.Year}");
-            }
-            Console.WriteLine();
-
-            var pacientesPorIdade = pacientes.Where(p => (DateTime.Now.Year - p.DataNascimento.Year) >= 20 && (DateTime.Now.Year - p.DataNascimento.Year) <= 30);
-            Console.WriteLine("Pacientes com idade entre 20 e 30 anos:");
-            foreach (var paciente in pacientesPorIdade)
-            {
-                Console.WriteLine($"Nome: {paciente.Nome}, Idade: {DateTime.Now.Year - paciente.DataNascimento.Year}");
-            }
-            Console.WriteLine();
-
-            string sexoInformado = "Masculino";
-            var pacientesPorSexo = pacientes.Where(p => p.Sexo != null && p.Sexo.Equals(sexoInformado, StringComparison.OrdinalIgnoreCase));
-            Console.WriteLine($"Pacientes do sexo {sexoInformado}:");
-            foreach (var paciente in pacientesPorSexo)
-            {
-            Console.WriteLine($"Nome: {paciente.Nome}, Sexo: {paciente.Sexo ?? "Não especificado"}");
-            }
-
-            Console.WriteLine();
-
-            var pacientesOrdenados = pacientes.OrderBy(p => p.Nome);
-            Console.WriteLine("Pacientes em ordem alfabética:");
-            foreach (var paciente in pacientesOrdenados)
-            {
-                Console.WriteLine($"Nome: {paciente.Nome}");
-            }
-            Console.WriteLine();
-
-            string textoSintoma = "Dor";
-            var pacientesPorSintoma = pacientes.Where(p => p.Sintomas != null && p.Sintomas.Contains(textoSintoma, StringComparison.OrdinalIgnoreCase));
-            Console.WriteLine($"Pacientes com sintomas contendo '{textoSintoma}':");
-            foreach (var paciente in pacientesPorSintoma)
-            {
-    Console.WriteLine($"Nome: {paciente.Nome}, Sintomas: {paciente.Sintomas ?? "Sintomas não especificados"}");
-            }
-
-            Console.WriteLine();
-
-            int mesInformado = 5;
-            var aniversariantes = medicos.Where(m => m.DataNascimento.Month == mesInformado)
-                                    .Cast<Pessoa>()
-                                    .Union(pacientes.Where(p => p.DataNascimento.Month == mesInformado)
-                                    .Cast<Pessoa>());
-            Console.WriteLine($"Aniversariantes do mês {mesInformado}:");
-            foreach (var pessoa in aniversariantes)
-            {
-                if (pessoa is Medico med)
-                {
-                    Console.WriteLine($"Médico: {med.Nome}, Dia do aniversário: {med.DataNascimento.Day}");
-                }
-                else if (pessoa is Paciente pac)
-                {
-                    Console.WriteLine($"Paciente: {pac.Nome}, Dia do aniversário: {pac.DataNascimento.Day}");
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+            Console.WriteLine($"Nome: {medico.Nome}, CRM: {medico.CRM}, Idade: {medico.Idade}");
         }
     }
-}
 
+    static void ExibirPacientes(IEnumerable<Paciente> pacientes)
+    {
+        foreach (var paciente in pacientes)
+        {
+            Console.WriteLine($"Nome: {paciente.Nome}, Sexo: {paciente.Sexo}, Idade: {paciente.Idade}");
+        }
+    }
+
+    static void ExibirMedicosEClientes(IEnumerable<Pessoa> pessoas)
+    {
+        foreach (var pessoa in pessoas)
+        {
+            if (pessoa is Medico medico)
+            {
+                Console.WriteLine($"Médico - Nome: {medico.Nome}, CRM: {medico.CRM}, Idade: {medico.Idade}");
+            }
+            else if (pessoa is Paciente paciente)
+            {
+                Console.WriteLine($"Paciente - Nome: {paciente.Nome}, Sexo: {paciente.Sexo}, Idade: {paciente.Idade}");
+            }
+        }
+    }
+
+    static void Main()
+    {
+        
+        ClinicaMedica clinica = new ClinicaMedica();
+
+       
+        clinica.AdicionarMedico(new Medico("Dr. Iann Asaphe", new DateTime(1980, 1, 1), "12345678901", "CRM85965"));
+
+        clinica.AdicionarMedico(new Medico("Dra. Lara Silveira", new DateTime(1975, 5, 10), "98765432101", "CRM54321"));
+
+
+        
+        clinica.AdicionarPaciente(new Paciente("Paciente: Roberto Luíz", new DateTime(1990, 3, 15), "11122233344", Sexo.Masculino, "Febre, Dor de cabeça"));
+
+        clinica.AdicionarPaciente(new Paciente("Paciente: Maria José", new DateTime(1985, 8, 20), "55566677788", Sexo.Feminino, "Dor nas costas"));
+        
+     clinica.AdicionarPaciente(new Paciente("Paciente: Danilo Silveira", new DateTime(1985, 6, 6), "55545677788", Sexo.Feminino, "Dor de Barriga"));
+
+     clinica.AdicionarPaciente(new Paciente("Paciente: Roberta Nascimento ", new DateTime(1985, 8, 20), "55566977788", Sexo.Feminino, "Dente"));
+
+
+        Console.WriteLine("Relatório de Médicos com idade entre 30 e 50 anos:");
+
+        var relatorioMedicosIdade = clinica.ObterMedicosPorIdade(30, 50);
+
+        ExibirMedicos(relatorioMedicosIdade);
+
+        Console.WriteLine("\nRelatório de Pacientes com idade entre 25 e 40 anos:");
+
+        var relatorioPacientesIdade = clinica.ObterPacientesPorIdade(25, 40);
+
+        ExibirPacientes(relatorioPacientesIdade);
+
+        Console.WriteLine("\nRelatório de Pacientes do sexo Masculino:");
+
+        var relatorioPacientesSexo = clinica.ObterPacientesPorSexo(Sexo.Masculino);
+
+        ExibirPacientes(relatorioPacientesSexo);
+
+        Console.WriteLine("\nRelatório de Pacientes em ordem alfabética:");
+
+        var relatorioPacientesOrdemAlfabetica = clinica.ObterPacientesOrdemAlfabetica();
+
+        ExibirPacientes(relatorioPacientesOrdemAlfabetica);
+
+        Console.WriteLine("\nRelatório de Pacientes com sintomas contendo 'Dor':");
+
+        var relatorioPacientesSintomas = clinica.ObterPacientesPorSintomas("Dor");
+
+        ExibirPacientes(relatorioPacientesSintomas);
+
+        Console.WriteLine("\nRelatório de Médicos e Pacientes aniversariantes do mês 5:");
+
+        var relatorioAniversariantes = clinica.ObterAniversariantesDoMes(5);
+
+        ExibirMedicosEClientes(relatorioAniversariantes);
+    }
+}
